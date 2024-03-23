f71948cb5755409407cfaf48f214859cd012636e
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-03-23 17:32:36 -0300

    fix en header

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { menuItems } from "./data/db";
+
+console.log(menuItems);
 function App() {
   return (
     <>
@@ -6,6 +9,16 @@ function App() {
           Calculadora de propina y consumo
         </h1>
       </header>
+
+      <main className="max-w-6xl mx-auto py-20 grid sm:grid-cols-2">
+        <div className="text-center bg-red-500">
+          <h2>Menu</h2>
+        </div>
+
+        <div className="text-center bg-red-800">
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   );
 }
