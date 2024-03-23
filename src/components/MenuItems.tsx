792cd2f06b7c48468e5fc07ab1591397a4e18f09
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-03-23 19:13:24 -0300

    creando hook useOrder

--- src/components/MenuItems.tsx
+++ src/components/MenuItems.tsx
@@ -8,7 +8,7 @@ type MenuItemPros = {
 export const MenuItems = ({ item }: MenuItemPros) => {
     return (
         <button
-            className="border-2 border-green-600 hover:bg-slate-300 w-full p-3 flex justify-between mb-2 mt-6"
+            className="border-2 rounded-lg border-green-600 hover:bg-slate-300 w-full p-3 flex justify-between mb-2 mt-6"
         >
             <p>{item.name}</p>
             <p className="font-black">${" "}{item.price}</p>
---
+++ src/hooks/useOrder.ts
@@ -0,0 +1,12 @@
+import { useState } from "react"
+import { OrderItem } from '../types/index';
+
+export const useOrder = () => {
+
+    const [order, setOrder: ] = useState<OrderItem[]>([])
+
+
+    return {
+
+    }
+}
