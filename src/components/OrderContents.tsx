331d8f99e5fe450bf6086484583bac45dae4ea0f
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-03-25 18:15:47 -0300

    fix renderizando el consumo

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -11,15 +11,25 @@ export const OrderContents = ({ order }: OrderContentsProps) => {
     <div>
       <h2 className="font-black text-4xl mx-3">Consumo</h2>
 
-      <div className="space-y-3 mt-6 mx-3">
+      <div className="space-y-3 mt-10 mx-3">
         {order.length === 0 ?
           <p className="text-center">La orden esta vacia</p>
           : (
             order.map(item => (
-              <div key={item.id}>
-                <p>
-                  {item.name}
-                </p>
+              <div className="flex justify-between border-t last-of-type:border-b border-teal-300  rounded-lg py-5 mx-auto items-center"
+                key={item.id}>
+                <div>
+
+                  <p className="text-lg">
+                    {item.name} - $ {item.price}
+                  </p>
+                  <p className="font-black flex justify-between">
+                    Cantidad: {item.quantity} - $ {item.price * item.quantity}
+                  </p>
+                </div>
+                <button className="bg-red-600 rounded-full w-8 h-8 text-white font-black">
+                  x
+                </button>
               </div>
             ))
 
