8aeed486a46610dbc1e15aa3906f917641dd36f4
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-03-25 22:22:19 -0300

    fix orderTotal

--- src/components/OrderTotal.tsx
+++ src/components/OrderTotal.tsx
@@ -3,13 +3,18 @@ import { OrderItem } from "../types"
 
 type OrderTotalProps = {
     order: OrderItem[]
+    tip: number
 
 }
 
 
-export const OrderTotal = ({ order }: OrderTotalProps) => {
+export const OrderTotal = ({ order, tip }: OrderTotalProps) => {
 
-    const subtotal = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
+    const subtotalSuma = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
+
+    const tipSuma = useMemo(() => subtotalSuma * tip, [tip, order])
+
+    const totalSuma = useMemo(() => subtotalSuma + tipSuma, [tip, order])
 
     return (
 
@@ -17,13 +22,13 @@ export const OrderTotal = ({ order }: OrderTotalProps) => {
             <div className="space-y-3">
                 <h2 className="font-black text-2xl">Totales y Propina:</h2>
                 <p>Subtotal a pagar:{" "}
-                    <span className="font-bold">${subtotal}</span>
+                    <span className="font-bold">${" "}{subtotalSuma}</span>
                 </p>
                 <p>Propina:{" "}
-                    <span className="font-bold">$0</span>
+                    <span className="font-bold">${" "}{tipSuma}</span>
                 </p>
                 <p>Total a pagar:{" "}
-                    <span className="font-bold">$0</span>
+                    <span className="font-bold">${" "}{totalSuma}</span>
                 </p>
             </div>
         </>
